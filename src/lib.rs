80a356060fc8f67fe473c468400ac8c9b0bd4c53
David Tolnay <dtolnay@gmail.com>
2019-04-21 16:01:19 -0700

    Show example code in rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,96 @@
 //!     for exactly one task at a time and deferred until replacement of the
 //!     realtime task for other tasks.
 //!
+//! # Example
+//!
+//! This example uses oqueue to sequence the output of worker threads on a Rayon
+//! threadpool.
+//!
+//! ```
+//! use oqueue::{Color::Red, Sequencer, Task};
+//! use rayon::ThreadPoolBuilder;
+//! use std::error::Error;
+//! use std::fs;
+//! use std::path::{Path, PathBuf};
+//! use std::thread;
+//! use std::time::Duration;
+//!
+//! type Result<T> = std::result::Result<T, Box<dyn Error>>;
+//!
+//! fn main() -> Result<()> {
+//!     // Come up with some work that needs to be performed. Let's pretend to
+//!     // perform work on each file in the current directory.
+//!     let mut files = Vec::new();
+//!     for entry in fs::read_dir(".")? {
+//!         files.push(entry?.path());
+//!     }
+//!     files.sort();
+//!
+//!     // Build a thread pool with one thread per cpu.
+//!     let cpus = num_cpus::get();
+//!     let pool = ThreadPoolBuilder::new().num_threads(cpus).build()?;
+//!
+//!     // Spin up the right number of worker threads. They will write to stderr.
+//!     let oqueue = Sequencer::stderr();
+//!     pool.scope(|scope| {
+//!         for _ in 0..cpus {
+//!             scope.spawn(|_| worker(&oqueue, &files));
+//!         }
+//!     });
+//!
+//!     Ok(())
+//! }
+//!
+//! fn worker(oqueue: &Sequencer, inputs: &[PathBuf]) {
+//!     // Perform tasks indicated by the sequencer.
+//!     loop {
+//!         let task = oqueue.begin();
+//!         match inputs.get(task.index) {
+//!             Some(path) => work(task, path),
+//!             None => return,
+//!         }
+//!     }
+//! }
+//!
+//! fn work(task: Task, path: &Path) {
+//!     // Produce output by writing to the task.
+//!     write!(task, "evaluating ");
+//!     task.bold();
+//!     writeln!(task, "{}", path.display());
+//!
+//!     // Do some expensive work...
+//!     let string = path.to_string_lossy();
+//!     thread::sleep(Duration::from_millis(150 * string.len() as u64));
+//!
+//!     // ... which may fail or succeed.
+//!     if string.contains('c') {
+//!         task.bold_color(Red);
+//!         write!(task, "  ERROR");
+//!         task.reset_color();
+//!         writeln!(task, ": path contains the letter 'c'");
+//!     }
+//! }
+//! ```
+//!
+//! The output of this program is guaranteed to display tasks in the intended
+//! sorted order and non-interleaved. Tasks will make progress in parallel
+//! without needing to wait to perform output. All output will appear the
+//! earliest possible including one task in real time at all times.
+//!
+//! <pre>
+//! evaluating <b>./.git</b>
+//! evaluating <b>./.gitignore</b>
+//! evaluating <b>./.travis.yml</b>
+//! evaluating <b>./Cargo.lock</b>
+//!   <b><font color="#dd0000">ERROR</font></b>: path contains the letter 'c'
+//! evaluating <b>./Cargo.toml</b>
+//! evaluating <b>./LICENSE-APACHE</b>
+//! evaluating <b>./LICENSE-MIT</b>
+//! evaluating <b>./examples</b>
+//! evaluating <b>./src</b>
+//!   <b><font color="#dd0000">ERROR</font></b>: path contains the letter 'c'
+//! evaluating <b>./target</b>
+//! </pre>
 
 #![cfg_attr(oqueue_doc_cfg, feature(doc_cfg))]
 
